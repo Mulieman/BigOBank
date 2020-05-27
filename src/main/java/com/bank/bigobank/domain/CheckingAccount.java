d1de66a58dc80799bc48c2d60130b16002eaee04
Mulieman <57609006+Mulieman@users.noreply.github.com>
2020-05-26 23:49:50 -0500

    Account create

--- src/main/java/com/bank/bigobank/domain/CheckingAccount.java
+++ src/main/java/com/bank/bigobank/domain/CheckingAccount.java
@@ -1,4 +1,7 @@
 package com.bank.bigobank.domain;
 
+import javax.persistence.Entity;
+
+@Entity
 public class CheckingAccount extends Account {
 }
---
+++ src/main/java/com/bank/bigobank/service/SavingAccountService.java
@@ -0,0 +1,10 @@
+package com.bank.bigobank.service;
+
+import com.bank.bigobank.repository.SavingAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SavingAccountService  extends  Account{
+
+}
