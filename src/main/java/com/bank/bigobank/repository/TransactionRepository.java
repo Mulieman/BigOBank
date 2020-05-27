5c88de18753535fc58a2c8ff9f3147f9ce241525
dave <dawit.girma@d>
2020-05-26 23:45:32 -0500

    @Repository annotation added

--- src/main/java/com/bank/bigobank/repository/TransactionRepository.java
+++ src/main/java/com/bank/bigobank/repository/TransactionRepository.java
@@ -2,9 +2,11 @@ package com.bank.bigobank.repository;
 
 import com.bank.bigobank.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
 
     List<Transaction> findByAccount(int acc_id);
